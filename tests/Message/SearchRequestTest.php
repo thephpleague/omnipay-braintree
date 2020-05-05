14a871f95ac7292063d3dfdd7c890f60469491b5
Zhanna Smolovich <zhanna.smolovich@theiconic.com.au>
2020-05-05 15:01:03 +1000

    Add test for request validation

--- tests/Message/SearchRequestTest.php
+++ tests/Message/SearchRequestTest.php
@@ -2,6 +2,7 @@
 
 namespace Omnipay\Braintree\Message;
 
+use Omnipay\Common\Exception\InvalidRequestException;
 use Omnipay\Tests\TestCase;
 
 class SearchRequestTest extends TestCase
@@ -30,4 +31,17 @@ class SearchRequestTest extends TestCase
         $this->assertSame('2187588535', $data['purchaseOrderNumber']);
     }
 
+    public function testValidateWillFailWIthNoOrderNumber()
+    {
+        $this->expectException(InvalidRequestException::class);
+
+        $request = new SearchRequest($this->getHttpClient(), $this->getHttpRequest(), \Braintree_Configuration::gateway());
+        $this->request->initialize(
+            array(
+                'randomParam' => '2187588535',
+            )
+        );
+        
+        $request->getData();
+    }
 }
